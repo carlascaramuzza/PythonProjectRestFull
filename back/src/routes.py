1d091fb0d9d5edecced55713fa5af5d6fe9c7de4
carlascaramuzza <carla_scaramuzza@hotmail.com>
2022-09-19 12:21:34 -0300

    Create a new endpoint in back

--- back/src/routes.py
+++ back/src/routes.py
@@ -4,9 +4,21 @@ from config import config
 
 from flask_mysqldb import MySQL
 
+
 app = Flask("Restaurante")
 
 conexao = MySQL(app)
+
+def username_validation(cpf):
+    cursor = conexao.connection.cursor()
+    sql = "SELECT cpf from usuario;"
+    cursor.execute(sql)
+    cpfs = cursor.fetchall()
+    for item in cpfs:
+        if item != cpf:
+            return 1
+        else: 
+            return None
 
 @app.route('/usuarios', methods=["GET"])
 def listar_usuarios():
@@ -39,6 +51,41 @@ def listar_curso(id):
     except Exception as ex:
         return jsonify({'mensagem': "Error"})
 
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        cursor = conexao.connection.cursor()
+        sql = """SELECT cpf, senha  FROM usuario 
+        WHERE cpf = '{0}' and senha = '{1}';""".format(request.json['cpf'],request.json['senha'])
+        cursor.execute(sql) 
+        dados = cursor.fetchone()
+        if dados != None:
+            #IMPLEMENTAR---------redirecionar para Home page
+            return jsonify({'mensagem': "LOGIN REALIZADO COM SUCESSO"})
+        else:
+            return jsonify({'mensagem': "FALHA NO LOGIN: Usuario não encontrado!"})    
+
+    except Exception as ex:
+        return jsonify({'mensagem': "Error"})
+
+@app.route('/usuarios', methods=['POST'])   #não esquecer q no front tem q bloquear os campos senha e cpf com nros exato 10 e 11 e tefone 11
+def  inserir_usuario():
+    try:           
+            cursor = conexao.connection.cursor()
+            sql= """INSERT INTO usuario (nome, username, genero, cpf, telefone, email, senha, tipo_usuario_id) VALUES
+            ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}');""".format(request.json['nome'],request.json['username'],
+            request.json['genero'],request.json['cpf'],request.json['telefone'],request.json['email'],request.json['senha'],request.json['tipo_usuario_id'])
+            cursor.execute(sql)
+            validation = username_validation(request.json['cpf'])
+            if validation == 1:
+                print('pasou')                
+                conexao.connection.commit()
+                print('pasou') 
+                return jsonify({'mensagem': "CADSATRO REALIZADO COM SUCESSO"})
+            else: 
+                return jsonify({'mensagem': "CPF JÁ EXISTENTE! POR FAVOR INSIRA OUTRO CPF."})
+    except Exception as ex:
+        return jsonify({'mensagem': "Error"})  
 
 def pagina_nao_encontrada(error):
     return "<h1>Página buscada não existe</h1>"
