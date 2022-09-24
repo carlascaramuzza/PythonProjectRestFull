9231da4c3ed67df81bbe44312162489f62af5097
carlascaramuzza <carla_scaramuzza@hotmail.com>
2022-09-23 21:42:19 -0300

    Endpoint test com ORM funcionando - relacionamento

--- back/src/models/entidades.py
+++ back/src/models/entidades.py
@@ -1,55 +1,82 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app import app
 
-app = Flask(__name__)
+#app = Flask(__name__)
 db = SQLAlchemy(app)
 
 class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer(), primary_key = True)
     nome = db.Column(db.String(50))
     genero = db.Column(db.String(1))
     cpf = db.Column(db.String(11))
-    telefone = db.Column(db.Integer(15))
+    telefone = db.Column(db.Integer())
     email = db.Column(db.String(50))
     senha = db.Column(db.String(50))
-    tipoUsuario = db.relationship('TipoUsuario', backref = 'usuario')
-    pedidoId = db.Column(db.Integer, db.ForeignKey('pedido.id'))
+    #tipoUsuario = db.relationship('TipoUsuario', backref = 'usuario')
+    #pedidoId = db.Column(db.Integer(), db.ForeignKey('pedido.id'))# aquia   a mesma coisa dos outros tabela 1 : * 1 não recebe fk da muitos
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'genero': self.genero,
+            'cpf': self.cpf,
+            'telefone': self.telefone,
+            'email': self.email,
+            'senha' : self.senha,
+            #'tipoUsuario' : self.tipoUsuario,
+            #'pedidoId' : self.pedidoId
+        }
+
+ #ENDERECO
+    
 
 class TipoUsuario(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(100))
-    usuarioId = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    #usuarioId = db.Column(db.Integer, db.ForeignKey('usuario.id'))# aquia   a mesma coisa dos outros tabela 1 : * 1 não recebe fk da muitos
 
 class Pedido(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    usuario = db.relationship('Usuario', backref = 'pedido')
+   # usuario = db.relationship('Usuario', backref = 'pedido')
     dataPedido = db.Column(db.DateTime)
     delivery = db.Column(db.Integer)
     valorTotal = db.Column(db.Float)
     endereco = db.Column(db.String(150))
     observacoes = db.Column(db.String(50))
-    statusPedido = db.relationship('StatusPedido', backref = 'pedido')
-    itemPedido = db.Column(db.Integer, db.ForeignKey('itemPedido.id'))
+   # statusPedido = db.relationship('StatusPedido', backref = 'pedido')
+    #itemPedido = db.Column(db.Integer, db.ForeignKey('itemPedido.id')) #porque foi criado esse ?? o relacionamento é 1:* é a tabela de item q recebe o id..pq pode ter mais de 1 item
 
 class StatusPedido(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     statusPedido = db.Column(db.String(50))
-    pedidoId = db.Column(db.integer, db.ForeignKey('pedido.id'))
+   # pedidoId = db.Column(db.Integer(), db.ForeignKey('pedido.id'))# mesma coisa eh data 1:* a tabela 1 não recebe o Id de muitos.. é uam taberla de apoio
 
 class ItemPedido(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    pedidoId = db.relationship('Pedido', backref = 'itemPedido')
-    produtoId = db.relationship('Produto', backref = 'itemPedido')
+    #pedidoId = db.relationship('Pedido', backref = 'itemPedido')
+    #produtoId = db.relationship('Produto', backref = 'itemPedido')
     valorUnitario = db.Column(db.Float)
     quantidade = db.Column(db.Integer)
     valorTotal = db.Column(db.Float)
-    
+
+ 
+#CATEGORIA
+
+
 class Produto(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(255))
     preco = db.Column(db.Float)
     categoria = db.Column(db.String(255))
-    itemPedido = db.Column(db.Integer, db.ForeignKey('itemPedido.id'))
+    #itemPedido = db.Column(db.Integer, db.ForeignKey('itemPedido.id')) #a mesma coisa aqui????
+
+
+
+#MESA
+
+#RESERVA MESA
 
 
 
--- back/src/routes.py
+++ back/src/routes.py
@@ -6,94 +6,101 @@ from flask_mysqldb import MySQL
 
 from app import app
 
+from models.entidades import Usuario
+
 
 #app = Flask("Restaurante")
 
-conexao = MySQL(app)
+#conexao = MySQL(app)
 
-def username_validation(cpf):
-    cursor = conexao.connection.cursor()
-    sql = "SELECT cpf from usuario;"
-    cursor.execute(sql)
-    cpfs = cursor.fetchall()
-    for item in cpfs:
-        if item != cpf:
-            return 1
-        else: 
-            return None
+# def username_validation(cpf):
+#     cursor = conexao.connection.cursor()
+#     sql = "SELECT cpf from usuario;"
+#     cursor.execute(sql)
+#     cpfs = cursor.fetchall()
+#     for item in cpfs:
+#         if item != cpf:
+#             return 1
+#         else: 
+#             return None
 
-@app.route('/usuarios', methods=["GET"])
-def listar_usuarios():
-    try:
-        cursor = conexao.connection.cursor()
-        sql = "SELECT u.id, u.nome, u.username, u.genero, u.telefone, u.email, tu.nome as tipo_usuario  FROM usuario u INNER JOIN tipo_usuario tu ON tu.id = u.tipo_usuario_id;"
-        cursor.execute(sql)
-        dados = cursor.fetchall()
-        usuarios = []
-        for fila in dados:
-            usuario = {'id': fila[0],'nome': fila[1],'username': fila[2], 'genero': fila[3], 'telefone': fila[4], 'email': fila[5], 'tipo_usuario': fila[6]}
-            usuarios.append(usuario)
-        return jsonify({'usuarios': usuarios, 'mensagem': "Lista de Cursos"})
-    except Exception as ex:
-        return jsonify({'mensagem': "Error"})  
+# @app.route('/usuarios', methods=["GET"])
+# def listar_usuarios():
+#     try:
+#         cursor = conexao.connection.cursor()
+#         sql = "SELECT u.id, u.nome, u.username, u.genero, u.telefone, u.email, tu.nome as tipo_usuario  FROM usuario u INNER JOIN tipo_usuario tu ON tu.id = u.tipo_usuario_id;"
+#         cursor.execute(sql)
+#         dados = cursor.fetchall()
+#         usuarios = []
+#         for fila in dados:
+#             usuario = {'id': fila[0],'nome': fila[1],'username': fila[2], 'genero': fila[3], 'telefone': fila[4], 'email': fila[5], 'tipo_usuario': fila[6]}
+#             usuarios.append(usuario)
+#         return jsonify({'usuarios': usuarios, 'mensagem': "Lista de Cursos"})
+#     except Exception as ex:
+#         return jsonify({'mensagem': "Error"})  
 
-@app.route('/usuarios/<id>', methods=['GET'])
-def listar_curso(id):
-    try:
-        cursor = conexao.connection.cursor()
-        sql = "SELECT u.id, u.nome, u.username, u.genero, u.telefone, u.email, tu.nome as tipo_usuario  FROM usuario u INNER JOIN tipo_usuario tu ON tu.id = u.tipo_usuario_id WHERE u.id = '{0}';".format(id)
-        cursor.execute(sql) 
-        dados = cursor.fetchone()
-        if dados != None:
-             usuario = {'id': dados[0],'nome': dados[1],'username': dados[2], 'genero': dados[3], 'telefone': dados[4], 'email': dados[5], 'tipo_usuario': dados[6]}
-             return jsonify({'usuarios': usuario, 'mensagem': "Usuario Encontrado!"})
-        else:
-             return jsonify({'mensagem': "Usuario não encontrado!"})    
+@app.route("/teste", methods=["GET"])
+def get_teste():
+   usuarios = Usuario.query.all()
+   return jsonify([usuario.to_json() for usuario in usuarios])
 
-    except Exception as ex:
-        return jsonify({'mensagem': "Error"})
+# @app.route('/usuarios/<id>', methods=['GET'])
+# def listar_curso(id):
+#     try:
+#         cursor = conexao.connection.cursor()
+#         sql = "SELECT u.id, u.nome, u.username, u.genero, u.telefone, u.email, tu.nome as tipo_usuario  FROM usuario u INNER JOIN tipo_usuario tu ON tu.id = u.tipo_usuario_id WHERE u.id = '{0}';".format(id)
+#         cursor.execute(sql) 
+#         dados = cursor.fetchone()
+#         if dados != None:
+#              usuario = {'id': dados[0],'nome': dados[1],'username': dados[2], 'genero': dados[3], 'telefone': dados[4], 'email': dados[5], 'tipo_usuario': dados[6]}
+#              return jsonify({'usuarios': usuario, 'mensagem': "Usuario Encontrado!"})
+#         else:
+#              return jsonify({'mensagem': "Usuario não encontrado!"})    
 
-@app.route('/login', methods=['POST'])
-def login():
-    try:
-        cursor = conexao.connection.cursor()
-        sql = """SELECT cpf, senha  FROM usuario 
-        WHERE cpf = '{0}' and senha = '{1}';""".format(request.json['cpf'],request.json['senha'])
-        cursor.execute(sql) 
-        dados = cursor.fetchone()
-        if dados != None:
-            #IMPLEMENTAR---------redirecionar para Home page
-            return jsonify({'mensagem': "LOGIN REALIZADO COM SUCESSO"})
-        else:
-            return jsonify({'mensagem': "FALHA NO LOGIN: Usuario não encontrado!"})    
+#     except Exception as ex:
+#         return jsonify({'mensagem': "Error"})
 
-    except Exception as ex:
-        return jsonify({'mensagem': "Error"})
+# @app.route('/login', methods=['POST'])
+# def login():
+#     try:
+#         cursor = conexao.connection.cursor()
+#         sql = """SELECT cpf, senha  FROM usuario 
+#         WHERE cpf = '{0}' and senha = '{1}';""".format(request.json['cpf'],request.json['senha'])
+#         cursor.execute(sql) 
+#         dados = cursor.fetchone()
+#         if dados != None:
+#             #IMPLEMENTAR---------redirecionar para Home page
+#             return jsonify({'mensagem': "LOGIN REALIZADO COM SUCESSO"})
+#         else:
+#             return jsonify({'mensagem': "FALHA NO LOGIN: Usuario não encontrado!"})    
 
-@app.route('/usuarios', methods=['POST'])   #não esquecer q no front tem q bloquear os campos senha e cpf com nros exato 10 e 11 e telefone 11
-def  inserir_usuario():
-    try:           
-            cursor = conexao.connection.cursor()
-            sql= """INSERT INTO usuario (nome, username, genero, cpf, telefone, email, senha, tipo_usuario_id) VALUES
-            ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}');""".format(request.json['nome'],request.json['username'],
-            request.json['genero'],request.json['cpf'],request.json['telefone'],request.json['email'],request.json['senha'],request.json['tipo_usuario_id'])
-            cursor.execute(sql)
-            validation = username_validation(request.json['cpf'])
-            if validation == 1:
-                print('pasou')                
-                conexao.connection.commit()
-                print('pasou') 
-                return jsonify({'mensagem': "CADSATRO REALIZADO COM SUCESSO"})
-            else: 
-                return jsonify({'mensagem': "CPF JÁ EXISTENTE! POR FAVOR INSIRA OUTRO CPF."})
-    except Exception as ex:
-        return jsonify({'mensagem': "Error"})  
+#     except Exception as ex:
+#         return jsonify({'mensagem': "Error"})
 
-def pagina_nao_encontrada(error):
-    return "<h1>Página buscada não existe</h1>"
+# @app.route('/usuarios', methods=['POST'])   #não esquecer q no front tem q bloquear os campos senha e cpf com nros exato 10 e 11 e telefone 11
+# def  inserir_usuario():
+#     try:           
+#             cursor = conexao.connection.cursor()
+#             sql= """INSERT INTO usuario (nome, username, genero, cpf, telefone, email, senha, tipo_usuario_id) VALUES
+#             ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}');""".format(request.json['nome'],request.json['username'],
+#             request.json['genero'],request.json['cpf'],request.json['telefone'],request.json['email'],request.json['senha'],request.json['tipo_usuario_id'])
+#             cursor.execute(sql)
+#             validation = username_validation(request.json['cpf'])
+#             if validation == 1:
+#                 print('pasou')                
+#                 conexao.connection.commit()
+#                 print('pasou') 
+#                 return jsonify({'mensagem': "CADSATRO REALIZADO COM SUCESSO"})
+#             else: 
+#                 return jsonify({'mensagem': "CPF JÁ EXISTENTE! POR FAVOR INSIRA OUTRO CPF."})
+#     except Exception as ex:
+#         return jsonify({'mensagem': "Error"})  
+
+# def pagina_nao_encontrada(error):
+#     return "<h1>Página buscada não existe</h1>"
    
 
 
-app.config.from_object(config['development'])   
-app.register_error_handler(404, pagina_nao_encontrada)
+#app.config.from_object(config['development'])   
+#app.register_error_handler(404, pagina_nao_encontrada)
 app.run()
